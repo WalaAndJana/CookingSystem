75bc6c28a1859436fb3dac5677a8aa8aa5caba80
JanaDroubi <jana.r.a.droubi@gmail.com>
2025-04-19 11:41:34 +0300

    jana

---
+++ src/test/java/ChefPackage/ChefNotificationTest.java
@@ -0,0 +1,40 @@
+package ChefPackage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChefNotificationTest {
+
+    @Test
+    void chefHasUpcomingMealPreparations() {
+    }
+
+    @Test
+    void setNotificationDetails() {
+    }
+
+    @Test
+    void scheduleUpdates() {
+    }
+
+    @Test
+    void verifyNotificationSent() {
+    }
+
+    @Test
+    void chefIsPreparingMeal() {
+    }
+
+    @Test
+    void setAlertDetails() {
+    }
+
+    @Test
+    void applyIngredientSubstitution() {
+    }
+
+    @Test
+    void verifyAlertSent() {
+    }
+}
